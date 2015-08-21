d56648af79d566ce29430022d491e9fe71725124
Peter Kenway <pkenway@gmail.com>
2015-08-20 18:53:20 -0700

    Minor code cleanup and some performance testing.

--- 1/phone_numbers.py
+++ 1/phone_numbers.py
@@ -1,15 +1,25 @@
 import array
 import random
 import time
-
 # Problem: sort an list of unique integers with a set maximum
 # Solution: use a bit array and flag the corresponding bit for each number.
 # this takes up extra space, but we can sort in O(n) time
 
+class CodeTimer:
+    def __init__(self, tag):
+        self.tag = tag
 
-# need a way to make an array of arbitrary size to store bit flags
-#can't really make a boolean array in python, so use 32 bit integers
+    def __enter__(self):
+        self.clock_start = time.clock()
+
+    def __exit__(self, type, value, traceback):
+        print ('%s - %s' % (self.tag, str(time.clock() - self.clock_start)))
+
+
 def makeBitArray(bitSize):
+    """Generate an array of bits initialized to 0.
+
+    Bits will be stored as 32-bit unsigned ints"""
     num_ints = bitSize  >> 5
 
     # will evaluate to true if the number of bits is not
@@ -25,22 +35,16 @@ def makeBitArray(bitSize):
     bit_array.extend((0,) * num_ints)
     return bit_array
 
-# get the bit at the given position
 def testBit(bit_array, bit_num):
+    """Retrieve the value at the given position."""
     record = bit_num >> 5
     offset = bit_num & 31
     mask = 1 << offset
-    try:
-        return bit_array[record] & mask
-    except:
-        print('error')
-        print(len(bit_array))
-        print(bit_num)
-        print(record)
-        print(offset)
-        raise
+    return bit_array[record] & mask
+   
 
 def setBit(bit_array, bit_num, is_selected):
+    """Set the value at the given position."""
     record = bit_num >> 5
     offset = bit_num & 31
     if is_selected:
@@ -53,7 +57,7 @@ def setBit(bit_array, bit_num, is_selected):
 
 
 def enter_numbers(numbers, maxNum):
-    # create a bit array with each number flagged
+    """Initialize a bit array and mark each number in the numbers list."""
     bit_array = makeBitArray(maxNum)
     for num in numbers:
         setBit(bit_array, num, True)
@@ -61,6 +65,7 @@ def enter_numbers(numbers, maxNum):
     return bit_array
 
 def get_all_numbers(bit_array, maxNum):
+    """Get all selected numbers for a bit array."""
     return [i for i in range(0, maxNum) if testBit(bit_array, i)]
 
 def print_all_numbers(bit_array, maxNum):
@@ -71,29 +76,27 @@ def print_all_numbers(bit_array, maxNum):
 def generate_unique_number_array(k, n):
     """ Generate k unique random integers with a max value of n."""
     # start with the first K integers selected
-    random_numbers = ([True,] * k)
-
-    # add slots for the rest
-    random_numbers.extend([False,] * (n - k))
-
-    # 
+    random_numbers = makeBitArray(n)
+    for i in range(0, k):
+        setBit(random_numbers, i, True)
+        
     for i in range(0, k):
         #exchange the current value with a value further in the array
         new_index = random.randrange(i+1, n, 1)
-        temp_val = random_numbers[new_index]
-        random_numbers[new_index] = random_numbers[i]
-        random_numbers[i] = temp_val
+
+        # need a temp val here
+        temp_bit = testBit(random_numbers, new_index)
+        setBit(random_numbers, new_index, testBit(random_numbers, i))
+        setBit(random_numbers, i, temp_bit)
 
     # now we have random numbers but they are ordered
     # first condense into an actual list of numbers
-    number_list = [i for i in range(0, n) if random_numbers[i]]
+    number_list = [i for i in range(0, n) if testBit(random_numbers,i)]
 
     # now shuffle the numbers
     for i in range(0, k - 1):
         dest = random.randrange(i+1, k,1)
-        temp_val = number_list[i]
-        number_list[i] = number_list[dest]
-        number_list[dest] = temp_val
+        number_list[i], number_list[dest] = number_list[dest], number_list[i]
 
     return number_list
 
@@ -101,18 +104,16 @@ if __name__ == '__main__':
 
     # timing it out, the algo didn't seem to shine 
     # over system sort until the numbers got this big
-    k = 10000000
-    n = 100000000
+    k = 1000000
+    n = 10000000
 
-    print('generating...')
-    rands = generate_unique_number_array(k, n)
-    print('done')
+    with CodeTimer('generating random numbers'):
+        rands = generate_unique_number_array(k, n)
+
     #compare performance to sort
-    sort_start =  (time.clock())
-    sorted_numbers = sorted(rands)
-    print('built-in sort: %s' % str(time.clock() - sort_start))
+    with CodeTimer('builtin sort'):
+        sorted_numbers = sorted(rands)
 
-    sort_start = time.clock()
+    with CodeTimer('bitmap sort'):
+        sorted_numbers = enter_numbers(rands, n)
 
-    sorted_numbers = enter_numbers(rands, n)
-    print('my sort: %s' % str(time.clock() - sort_start))
